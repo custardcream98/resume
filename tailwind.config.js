43f06da78e687c1be7425aca165e53da47b169d3
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-10-02 12:54:25 +0900

    Chore: 마크업 및 Tailwind 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Noto Sans KR", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      boxShadow: {
+        section: "0 0 60px -10px rgba(0, 0, 0, 0.3)",
+      },
+      colors: {
+        black: "#222222",
+        gray: "#E9E9E9",
+        white: "#F0F0F0",
+        blue1: "#0938F0",
+        blue2: "#094AF0",
+        blue3: "#0961F0",
+        blue4: "#0978F0",
+        blue5: "#03B8F4",
+      },
+    },
   },
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [require("prettier-plugin-tailwindcss"), "accessibility"],
+  // accessibility 플러그인을 이용하면 Tailwind에서 제공하는 sr-only 스타일을 사용할 수 있습니다.
 };
